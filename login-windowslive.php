cd9e506f78d1597e6bf38826ae4a9c095fab348a
Lars Schenk <info@lars-schenk.de>
2014-12-12 13:11:45 +0100

    Use site_url() instead of SERVER_NAME.

--- login-windowslive.php
+++ login-windowslive.php
@@ -9,7 +9,7 @@ define('HTTP_UTIL', get_option('wpoa_http_util'));
 define('CLIENT_ENABLED', get_option('wpoa_windowslive_api_enabled'));
 define('CLIENT_ID', get_option('wpoa_windowslive_api_id'));
 define('CLIENT_SECRET', get_option('wpoa_windowslive_api_secret'));
-define('REDIRECT_URI', "http://" . rtrim($_SERVER['SERVER_NAME'], "/") . "/");
+define('REDIRECT_URI', rtrim(site_url(), '/') . '/');
 define('SCOPE', 'wl.basic'); // PROVIDER SPECIFIC: 'wl.basic' is the minimum scope required to get the user's id from Windows Live
 define('URL_AUTH', "https://login.live.com/oauth20_authorize.srf?");
 define('URL_TOKEN', "https://login.live.com/oauth20_token.srf?");
