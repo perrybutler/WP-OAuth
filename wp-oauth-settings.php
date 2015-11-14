8319b019630755be29c306ce524df586f96d3011
Miguel Ángel Casanova <miguelangelcasanova@gmail.com>
2015-11-14 16:45:56 +0100

    Update wp-oauth-settings.php

--- wp-oauth-settings.php
+++ wp-oauth-settings.php
@@ -493,6 +493,47 @@
 			</div> <!-- .wpoa-settings-section -->
 			<!-- END User Registration section -->
 			
+			<!-- START Login with Fiware section -->
+			<div id="wpoa-settings-section-login-with-fiware" class="wpoa-settings-section">
+			<h3>Login with Fiware</h3>
+			<div class='form-padding'>
+			<table class='form-table'>
+				<tr valign='top'>
+				<th scope='row'>Enabled:</th>
+				<td>
+					<input type='checkbox' name='wpoa_fiware_api_enabled' value='1' <?php checked(get_option('wpoa_fiware_api_enabled') == 1); ?> />
+				</td>
+				</tr>
+				
+				<tr valign='top'>
+				<th scope='row'>Client ID:</th>
+				<td>
+					<input type='text' name='wpoa_fiware_api_id' value='<?php echo get_option('wpoa_fiware_api_id'); ?>' />
+				</td>
+				</tr>
+
+				<tr valign='top'>
+				<th scope='row'>Client Secret:</th>
+				<td>
+					<input type='text' name='wpoa_fiware_api_secret' value='<?php echo get_option('wpoa_fiware_api_secret'); ?>' />
+				</td>
+				</tr>
+			</table> <!-- .form-table -->
+			<p>
+				<strong>Instructions:</strong>
+				<ol>
+					<li>Visit the Fiware website <a href='https://console.developers.fiware.com/project' target="_blank">console.developers.fiware.com</a>.</li>
+					<li>At Fiware, create a new Project and enable the Fiware+ API. This will enable your site to access the Fiware+ API.</li>
+					<li>At Fiware, provide your site's homepage URL (<?php echo $blog_url; ?>) for the new Project's Redirect URI. Don't forget the trailing slash!</li>
+					<li>At Fiware, you must also configure the Consent Screen with your Email Address and Product Name. This is what fiware will display to users when they are asked to grant access to your site/app.</li>
+					<li>Paste your Client ID/Secret provided by Fiware into the fields above, then click the Save all settings button.</li>
+				</ol>
+			</p>
+			<?php submit_button('Save all settings'); ?>
+			</div> <!-- .form-padding -->
+			</div> <!-- .wpoa-settings-section -->
+			<!-- END Login with Fiware section -->
+			
 			<!-- START Login with Google section -->
 			<div id="wpoa-settings-section-login-with-google" class="wpoa-settings-section">
 			<h3>Login with Google</h3>
