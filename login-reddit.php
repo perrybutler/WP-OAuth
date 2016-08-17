434b1495fabef7ede7dd4e3226e7a1ba1169e4c5
Emmanuel CHAFFRAIX <emmanuel.chaffraix@margoconseil.com>
2016-08-17 13:56:37 +0200

    Refactor reddit

--- login-reddit.php
+++ login-reddit.php
@@ -1,184 +1,48 @@
 <?php
 
 include_once 'session.php';
+include_once 'login-common.php';
 
-# DEFINE THE OAUTH PROVIDER AND SETTINGS TO USE #
-WPOA_Session::set_provider('Reddit');
-define('HTTP_UTIL', get_option('wpoa_http_util'));
-define('CLIENT_ENABLED', get_option('wpoa_reddit_api_enabled'));
-define('CLIENT_ID', get_option('wpoa_reddit_api_id'));
-define('CLIENT_SECRET', get_option('wpoa_reddit_api_secret'));
-define('REDIRECT_URI', rtrim(site_url(), '/') . '/');
-define('SCOPE', 'identity'); // PROVIDER SPECIFIC: 'identity' is the minimum scope required to get the user's id from Reddit
-define('URL_AUTH', "https://ssl.reddit.com/api/v1/authorize?");
-define('URL_TOKEN', "https://ssl.reddit.com/api/v1/access_token?");
-define('URL_USER', "https://oauth.reddit.com/api/v1/me?");
-# END OF DEFINE THE OAUTH PROVIDER AND SETTINGS TO USE #
+error_reporting(E_ALL | E_STRICT);
 
-WPOA_Session::save_last_url();
+class LoginReddit extends AbstractLoginCommon {
+  public function __construct($wpoa) {
+    parent::__construct('Reddit', 
+      "https://ssl.reddit.com/api/v1/authorize?",
+      "https://ssl.reddit.com/api/v1/access_token?",
+      "https://oauth.reddit.com/api/v1/me",
+      $wpoa);
+    $this->useBearer = true;
+	$this->sendIdAndSecretInHeader = true;
+  }
 
-# AUTHENTICATION FLOW #
-// the oauth 2.0 authentication flow will start in this script and make several calls to the third-party authentication provider which in turn will make callbacks to this script that we continue to handle until the login completes with a success or failure:
-if (!CLIENT_ENABLED) {
-	$this->wpoa_end_login("This third-party authentication provider has not been enabled. Please notify the admin or try again later.");
-}
-elseif (!CLIENT_ID || !CLIENT_SECRET) {
-	// do not proceed if id or secret is null:
-	$this->wpoa_end_login("This third-party authentication provider has not been configured with an API key/secret. Please notify the admin or try again later.");
-}
-elseif (isset($_GET['error_description'])) {
-	// do not proceed if an error was detected:
-	$this->wpoa_end_login($_GET['error_description']);
-}
-elseif (isset($_GET['error_message'])) {
-	// do not proceed if an error was detected:
-	$this->wpoa_end_login($_GET['error_message']);
-}
-elseif (isset($_GET['code'])) {
-	// post-auth phase, verify the state:
-	if (WPOA_Session::get_state() == $_GET['state']) {
-		// get an access token from the third party provider:
-		get_oauth_token($this);
-		// get the user's third-party identity and attempt to login/register a matching wordpress user account:
-		$oauth_identity = get_oauth_identity($this);
-		$this->wpoa_login_user($oauth_identity);
-	}
-	else {
-		// possible CSRF attack, end the login with a generic message to the user and a detailed message to the admin/logs in case of abuse:
-		// TODO: report detailed message to admin/logs here...
-		$this->wpoa_end_login("Sorry, we couldn't log you in. Please notify the admin or try again later.");
-	}
-}
-else {
-	// pre-auth, start the auth process:
-	if ((empty(WPOA_Session::get_expires_at())) || (time() > WPOA_Session::get_expires_at())) {
-		// expired token; clear the state:
-		$this->wpoa_clear_login_state();
-	}
-	get_oauth_code($this);
-}
-// we shouldn't be here, but just in case...
-$this->wpoa_end_login("Sorry, we couldn't log you in. The authentication flow terminated in an unexpected way. Please notify the admin or try again later.");
-# END OF AUTHENTICATION FLOW #
+  
+  function getOAuthCodeExtendArray($array) {
+    $array['scope'] = 'identity';
+    return $array;
+  }
 
-# AUTHENTICATION FLOW HELPER FUNCTIONS #
-function get_oauth_code($wpoa) {
-	$params = array(
-		'response_type' => 'code',
-		'client_id' => CLIENT_ID,
-		'scope' => SCOPE,
-		'state' => uniqid('', true),
-		'redirect_uri' => REDIRECT_URI,
-	);
-	WPOA_Session::set_state($params['state']);
-	$url = URL_AUTH . http_build_query($params);
-	header("Location: $url");
-	exit;
+  function getOAuthTokenExtendArray($array) {
+    $array['client_id'] = null;
+    $array['client_secret'] = null;
+    
+    return $array;
+  }
+  
+  function getOAuthIdentityParameters(){
+    return array("schema" => "openid",
+                 "access_token" => WPOA_Session::get_token());
+  }
+  
+  function getOAuthIdentityFillArray($result_obj, $oauth_identity){
+    $oauth_identity[WPOA_Session::USER_ID] = $result_obj['id'];
+    $oauth_identity[WPOA_Session::USER_NAME] = $result_obj['name'];
+	// TODO : Reddit does not send email. If they implement it, we must update the following line.
+    $oauth_identity[WPOA_Session::USER_EMAIL] = $result_obj['name'] . "@reddit.com";
+
+    return $oauth_identity;
+  }
 }
 
-function get_oauth_token($wpoa) {
-	$params = array(
-		'grant_type' => 'authorization_code',
-		'client_id' => CLIENT_ID,
-		'client_secret' => CLIENT_SECRET,
-		'code' => $_GET['code'],
-		'redirect_uri' => REDIRECT_URI,
-	);
-	$url_params = http_build_query($params);
-	switch (strtolower(HTTP_UTIL)) {
-		case 'curl':
-			$url = URL_TOKEN . $url_params;
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl, CURLOPT_POST, 1);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-			curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // PROVIDER SPECIFIC: Reddit requires a User-Agent
-			curl_setopt($curl, CURLOPT_USERPWD, CLIENT_ID . ":" . CLIENT_SECRET); // PROVIDER SPECIFIC: Reddit requires basic authentication with this request
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, (get_option('wpoa_http_util_verify_ssl') == 1 ? 1 : 0));
-			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, (get_option('wpoa_http_util_verify_ssl') == 1 ? 2 : 0));
-			$result = curl_exec($curl);
-			break;
-		case 'stream-context':
-			$url = $wpoa->wpoa_add_basic_auth(rtrim(URL_TOKEN, "?"), CLIENT_ID, CLIENT_SECRET); // PROVIDER SPECIFIC: Reddit requires basic authentication with the request
-			$opts = array('http' =>
-				array(
-					'method'  => 'POST',
-					'header'  => 'Content-type: application/x-www-form-urlencoded',
-					'content' => $url_params,
-				)
-			);
-			$context = $context  = stream_context_create($opts);
-			$result = @file_get_contents($url, false, $context);
-			if ($result === false) {
-				$wpoa->wpoa_end_login("Sorry, we couldn't log you in. Could not retrieve access token via stream context. Please notify the admin or try again later.");
-			}
-			break;
-	}
-	// parse the result:
-	$result_obj = json_decode($result, true); // PROVIDER SPECIFIC: Reddit encodes the access token result as json by default
-	$access_token = $result_obj['access_token']; // PROVIDER SPECIFIC: this is how Reddit returns the access token KEEP THIS PROTECTED!
-	$expires_in = $result_obj['expires_in']; // PROVIDER SPECIFIC: this is how Reddit returns the access token's expiration
-	$expires_at = time() + $expires_in;
-	// handle the result:
-	if (!$access_token || !$expires_in) {
-		// malformed access token result detected:
-		$wpoa->wpoa_end_login("Sorry, we couldn't log you in. Malformed access token result detected. Please notify the admin or try again later.");
-	}
-	else {
-		WPOA_Session::set_token($access_token);
-		WPOA_Session::set_expires_in($expires_in);
-		WPOA_Session::set_expires_at($expires_at);
-		return true;
-	}
-}
-
-function get_oauth_identity($wpoa) {
-	// here we exchange the access token for the user info...
-	// set the access token param:
-	$params = array(
-		'access_token' => WPOA_Session::get_token(), // PROVIDER SPECIFIC: the access token is passed to Reddit using this key name
-	);
-	$url_params = http_build_query($params);
-	// perform the http request:
-	switch (strtolower(HTTP_UTIL)) {
-		case 'curl':
-			$url = rtrim(URL_USER, "?");
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // PROVIDER SPECIFIC: Reddit requires a User-Agent
-			curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: bearer " . WPOA_Session::get_token())); // PROVIDER SPECIFIC: the access token must be sent to Reddit as a bearer header
-			// PROVIDER NORMALIZATION: LinkedIn requires an x-li-format: json header...
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-			$result = curl_exec($curl);
-			$result_obj = json_decode($result, true);
-			break;
-		case 'stream-context':
-			$url = rtrim(URL_USER, "?");
-			$opts = array('http' =>
-				array(
-					'method'  => 'GET',
-					'user_agent' => $_SERVER['HTTP_USER_AGENT'], // PROVIDER SPECIFIC: Reddit requires a User-Agent
-					'header'  => "Authorization: bearer " . WPOA_Session::get_token(), // PROVIDER SPECIFIC: the access token must be sent to Reddit as a bearer header
-				)
-			);
-			$context = $context  = stream_context_create($opts);
-			$result = @file_get_contents($url, false, $context);
-			if ($result === false) {
-				$wpoa->wpoa_end_login("Sorry, we couldn't log you in. Could not retrieve user identity via stream context. Please notify the admin or try again later.");
-			}
-			$result_obj = json_decode($result, true);
-			break;
-	}
-	// parse and return the user's oauth identity:
-	$oauth_identity = array();
-	$oauth_identity[WPOA_Session::PROVIDER] = WPOA_Session::get_provider();
-	$oauth_identity[WPOA_Session::USER_ID] = $result_obj['id']; // PROVIDER SPECIFIC: this is how Reddit returns the user's unique id
-	//$oauth_identity[WPOA_Session::USER_NAME] = 'not_provided'; // PROVIDER SPECIFIC: Reddit never provides the user's email!
-	if (!$oauth_identity[WPOA_Session::USER_ID]) {
-		$wpoa->wpoa_end_login("Sorry, we couldn't log you in. User identity was not found. Please notify the admin or try again later.");
-	}
-	return $oauth_identity;
-}
-# END OF AUTHENTICATION FLOW HELPER FUNCTIONS #
-?>
+$login = new LoginReddit($this);
+$login->run();
