edcd1e91a5ba678a3f988d1876ed383a994930f0
momagu <moritz.morawietz@uni-oldenburg.de>
2016-10-15 21:34:34 +0200

    Added Login with Slack functionality

--- wp-oauth-settings.php
+++ wp-oauth-settings.php
@@ -528,6 +528,46 @@
 			</div> <!-- .wpoa-settings-section -->
 			<!-- END Login with Google section -->
 
+			<!-- START Login with Slack section -->
+			<div id="wpoa-settings-section-login-with-slack" class="wpoa-settings-section">
+				<h3>Login with Slack</h3>
+				<div class='form-padding'>
+					<table class='form-table'>
+						<tr valign='top'>
+							<th scope='row'>Enabled:</th>
+							<td>
+								<input type='checkbox' name='wpoa_slack_api_enabled' value='1' <?php checked(get_option('wpoa_slack_api_enabled') == 1); ?> />
+							</td>
+						</tr>
+
+						<tr valign='top'>
+							<th scope='row'>Client ID:</th>
+							<td>
+								<input type='text' name='wpoa_slack_api_id' value='<?php echo get_option('wpoa_slack_api_id'); ?>' />
+							</td>
+						</tr>
+
+						<tr valign='top'>
+							<th scope='row'>Client Secret:</th>
+							<td>
+								<input type='text' name='wpoa_slack_api_secret' value='<?php echo get_option('wpoa_slack_api_secret'); ?>' />
+							</td>
+						</tr>
+					</table> <!-- .form-table -->
+					<p>
+						<strong>Instructions:</strong>
+					<ol>
+						<li>Visit  <a href='https://api.slack.com/apps' target="_blank">https://api.slack.com/apps</a>.</li>
+						<li>At Slack, create a new Project and make sure the Development Slack Team is set to the one wich users shoulb be able to login</li>
+						<li>At Slack, OAuth & Permissions, provide your site's homepage URL (<?php echo $blog_url; ?>) for the new App's Redirect URI. Don't forget the trailing slash!</li>
+						<li>Paste your Client ID/Secret provided by Slack into the fields above, then click the Save all settings button.</li>
+					</ol>
+					</p>
+					<?php submit_button('Save all settings'); ?>
+				</div> <!-- .form-padding -->
+			</div> <!-- .wpoa-settings-section -->
+			<!-- END Login with Slack section -->
+
 			<!-- START Login with WP OAuth Server section -->
 			<div id="wpoa-settings-section-login-with-wp-oauth-server" class="wpoa-settings-section">
 			<h3>Login with WP OAuth Server</h3>
